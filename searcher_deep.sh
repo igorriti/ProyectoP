5e411200b4e8a713be7682e68d159b90ab12f6ea
Ignacio Gorriti <gorritiignacioe@gmail.com>
2023-08-30 12:20:39 -0300

    fix bugs ios

---
+++ searcher_deep.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Función para imprimir el uso correcto del script
+print_usage() {
+    echo "Uso: $0 directorio"
+    echo "Ejemplo: $0 /ruta/al/directorio"
+}
+
+# Verificar si se proporciona un directorio como argumento
+if [ $# -ne 1 ]; then
+    echo "Se necesita un directorio como argumento."
+    print_usage
+    exit 1
+fi
+
+# Verificar si el directorio existe
+if [ ! -d "$1" ]; then
+    echo "El directorio \"$1\" no existe."
+    exit 1
+fi
+
+# Obtener la lista de archivos y directorios con caracteres especiales en su nombre
+find "$1" -name "*[áéíóúñÁÉÍÓÚüÜ¿¡!@#$%^&*()_+={}\[\]:;\"'<>,.?/\\|`~]*" | while read -r item; do
+    echo "Archivo o directorio con caracteres especiales encontrado: $item"
+done
+
+echo "Búsqueda completada."
